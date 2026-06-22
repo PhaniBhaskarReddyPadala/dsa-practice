0b52883b4d9ec041e20433d68435bc8f1541b1af
PhaniBhaskaraReddyPadala <reddyphani040@gmail.com>
2026-06-22 11:34:32 +0530

    added a file from lpu-summer-pep to summer_pep

---
+++ summer_pep/ifelse.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int age=18;
+    // int c=10;
+    // if(age>18 && c>10){
+    //     cout<<"hi";
+    // }else {
+    //     cout << "h";
+    // }
+
+    char g='C';
+    switch(g){
+        case 'A':
+        cout << "e" << endl;
+        case 'B':
+        cout << "b" << endl;
+        case 'C':
+        cout << "c" << endl;
+        default:
+        cout << "invalid" << endl;
+    }
+    return 0;
+}
